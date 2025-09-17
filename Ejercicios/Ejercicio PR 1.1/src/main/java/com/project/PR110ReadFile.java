636fabcbee48edbe6c56f34d15fcf2fd863480ec
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-09-17 18:15:51 +0200

    Readfile
    Pasa el test, funciona, he usado Buffer Reader para leer cada linea de un documento

--- Ejercicios/Ejercicio PR 1.1/src/main/java/com/project/PR110ReadFile.java
+++ Ejercicios/Ejercicio PR 1.1/src/main/java/com/project/PR110ReadFile.java
@@ -1,5 +1,9 @@
 package com.project;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PR110ReadFile {
 
     public static void main(String[] args) {
@@ -9,6 +13,18 @@ public class PR110ReadFile {
 
     // Funció que llegeix el fitxer i mostra les línies amb numeració
     public static void llegirIMostrarFitxer(String camiFitxer) {
+        int numLinia = 1; // Numero a la izq 
+        try (BufferedReader br = new BufferedReader(new FileReader(camiFitxer))) { //Bufer que lee
+            String linia; // String vacio
+
+            while ((linia = br.readLine()) != null) { //Mientras haya lineas
+                System.out.println(numLinia + ": " + linia); //Mostrar la linea como se pide
+                numLinia++; //Siguiente linea
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error!");
+        }
 
     }
 }
