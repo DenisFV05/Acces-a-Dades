91a78a159fbb3084425c39b2edf34397eee2538b
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-09-23 19:25:38 +0200

    Avances en PR.1.1

--- Ejercicios/Ejercicio PR 1.1/src/main/java/com/project/PR111Files.java
+++ Ejercicios/Ejercicio PR 1.1/src/main/java/com/project/PR111Files.java
@@ -1,5 +1,8 @@
 package com.project;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class PR111Files {
 
     public static void main(String[] args) {
@@ -8,5 +11,43 @@ public class PR111Files {
     }
 
     public static void gestionarArxius(String camiFitxer) {
+        // 1. Crear carpeta System.getProperty("user.dir") + "/data/pr111/myFiles"
+        Path dir = Paths.get(camiFitxer, "myFiles");
+
+        try {
+            Files.createDirectories(dir);
+        // 2. Crear file1.txt file2.txt
+            Path file1 = dir.resolve("file1.txt");
+            Path file2 = dir.resolve("file2.txt");
+
+            if (!Files.exists(file1)) {
+                Files.createFile(file1);
+            }
+            if (!Files.exists(file2)) {
+                Files.createFile(file2);
+            }
+            
+        // 3. Rename file2.txt to renamedFile.txt
+            Path renamedFile = dir.resolve("renamedFile.txt");
+            if (Files.exists(file2)) {
+                Files.move(file2, renamedFile, StandardCopyOption.REPLACE_EXISTING); // Lo he buscado, basicamente lo reemplaza
+            }
+        // 4. List arxius dins de myFiles
+            System.out.println("Els arxius de la carpeta són:");
+            // p es cada file, getFilenName coge el nombre, y se imprime con un guion delante
+            Files.list(dir).forEach(p -> System.out.println(" - " + p.getFileName())); // Funcion que he buscado lambda
+        // 5. Delete file1.txt
+            if (Files.exists(file1)) {
+                Files.delete(file1);
+            }
+        // 6. List de nuevo
+            System.out.println("Els arxius de la carpeta són:");
+            // p es cada file, getFilenName coge el nombre, y se imprime con un guion delante
+            Files.list(dir).forEach(p -> System.out.println(" - " + p.getFileName())); // Funcion que he buscado lambda
+
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
--- Ejercicios/Ejercicio PR 1.1/src/main/java/com/project/PR112cat.java
+++ Ejercicios/Ejercicio PR 1.1/src/main/java/com/project/PR112cat.java
@@ -1,5 +1,11 @@
 package com.project;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+
 public class PR112cat {
 
     public static void main(String[] args) {
@@ -16,5 +22,21 @@ public class PR112cat {
 
     // Funció per mostrar el contingut de l'arxiu o el missatge d'error corresponent
     public static void mostrarContingutArxiu(String rutaArxiu) {
+        Path fitxer = Paths.get(rutaArxiu);
+        if (Files.exists(fitxer)) {
+            if (Files.isDirectory(fitxer)) {
+                System.out.println("El path no correspon a un arxiu, sinó a una carpeta.");
+                return; // Error si el archivo es una carpeta
+            }
+
+            try {
+                Files.lines(fitxer, StandardCharsets.UTF_8).forEach(System.out::println); // Imprime cada linea
+            } catch (IOException e) { // Error si el archivo no existe o otro problema
+                System.out.println("El fitxer no existeix o no és accessible.");
+            }
+
+        } else {
+            System.out.println("El fitxer no existeix o no és accessible.");
+        }
     }
 }
